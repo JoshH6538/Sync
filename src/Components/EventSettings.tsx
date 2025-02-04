975b38a8bbe6dcb6d62819486e1e87907d168a0f
JoshH6538 <JOSHUAHANSCOM6538@GMAIL.COM>
2025-02-04 12:46:11 -0800

    modified event settings form

--- src/Components/EventSettings.tsx
+++ src/Components/EventSettings.tsx
@@ -1,29 +1,28 @@
-import React from 'react';
 import '../Styles/EventSettings.css';
 
 export default function EventSettings() {
   return (
     <div id='event-settings-container'>
-        <form id='event-settings-form'>
+        <form id='event-settings-form' >
             <div id='radius-settings'>
                 <label className='settings-label'>Radius</label>
                 <input type='number' placeholder=' 10 - 1000' id='radius' step="10"></input>
                 <select id='radiusUnit' className='select-setting'>
-                    <option>miles</option>
-                    <option>km</option>
+                    <option value="miles">Miles</option>
+                    <option value="km">km</option>
                 </select>
             </div>
             <div id='sort-settings'>
               <label className='settings-label'>Sort By</label>
               <select id='sortObject' className='select-setting'>
-                  <option>name</option>
-                  <option>date</option>
-                  <option>relevance</option>
-                  <option>distance</option>
+                  <option value="name">Name</option>
+                  <option value="date">Date</option>
+                  <option value="relevance">Relevance</option>
+                  <option value="distance">Distance</option>
               </select>
               <select id='sortOrder' className='select-setting'>
-                  <option>asc</option>
-                  <option>desc</option>
+                  <option value="asc">Ascending</option>
+                  <option value="desc">Descending</option>
               </select>
             </div>
             <button type='submit' id='event-settings-submit'>Submit</button>
