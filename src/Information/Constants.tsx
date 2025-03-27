72fb3d32c249f0dfcf82d45c9b34f22077451945
JoshH6538 <JOSHUAHANSCOM6538@GMAIL.COM>
2025-03-27 11:03:29 -0700

    testing different href for tabicon

--- src/Information/Constants.tsx
+++ src/Information/Constants.tsx
@@ -2,7 +2,7 @@ const Constants = {
     // Authorization page
     SPOTIFY_AUTHORIZE_ENDPOINT: "https://accounts.spotify.com/authorize",
     //redirect back to site
-    REDIRECT_URL_AFTER_LOGIN: "https://joshh6538.github.io/Sync/",
+    REDIRECT_URL_AFTER_LOGIN: "https://joshh6538.github.io",
     // REDIRECT_URL_AFTER_LOGIN: "http://localhost:5173",
     //permissions from api at https://developer.spotify.com/documentation/web-api/concepts/scopes
     SCOPES: ["user-top-read"],
